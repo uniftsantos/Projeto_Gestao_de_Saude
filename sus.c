b22a753bc002618e911507c2a000bd5f6b633323
Tiago Fagundes dos Santos <tiagofagundesdossantos@gmail.com>
2024-10-27 21:18:13 -0300

    Criando as estruturas solicitadas para o desenvolvimneto do projeto.

---
+++ sus.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+//Declarando as estruturas solicitados para o desenvolvimento do projeto.
+
+typedef struct {
+    int dia;
+    int mes;
+    int ano;
+} Data;
+
+
+typedef struct {
+    char nome[100];
+    int idade;
+    char rg[15];
+    Data data;
+
+} Registro;
+
+int main(){
+
+
+
+}
